3f0a30af744037887ed2f7f16a6864b9c5d0ba90
Rianna Cantarelli <rianna.aeon@gmail.com>
2021-12-26 20:40:18 -0300

    Ajustes menores

--- make/makeEnvironment.sh
+++ make/makeEnvironment.sh
@@ -4,8 +4,9 @@
 # Variáveis para uso do Makefile
 MK_ROOT_PATH=${PWD}
 MK_MY_ENVIRONMENT_FILE="${MK_ROOT_PATH}/make/myMakeEnvironment.sh"
-MK_WEB_SERVER_ENV_FILE="${MK_ROOT_PATH}/container-config/apache-php-7.4/etc/.env"
 MK_WEB_SERVER_DATABASE_BOOTSTRAP_FILE="/etc/database/bootstrap.sql"
+
+MK_WEB_SERVER_ENV_FILE="${MK_ROOT_PATH}/container-config/apache-php-7.4/etc/.env"
 MK_LOCAL_BOOTSTRAP_FILE="${MK_ROOT_PATH}/container-config/apache-php-7.4${MK_WEB_SERVER_DATABASE_BOOTSTRAP_FILE}"
 MK_LOCAL_CONTAINER_ROOT_DIR="${MK_ROOT_PATH}/container-config/apache-php-7.4"
 
